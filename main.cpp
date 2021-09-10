75986455e5bbb47f83a9de34227371dcd56e96b2
JosuePomacusi <81737048+JosuePomacusi@users.noreply.github.com>
2021-09-10 01:43:16 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	cout << "Nombre: Josue Fabricio Pomacusi Martinez" << endl;
+	cout << "Carrera: Ingenieria de Sistemas" << endl;
+	cout << "CI: 10391008" << endl;
+	cout << "CU: 35-5083" << endl;
+
+	return 0;
+}
